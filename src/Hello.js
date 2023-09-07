aaa6f87b1be10d67017b751a11048c74ddd7daab
vitchii <vitchiiana@gmail.com>
2023-09-07 17:23:22 +0100

    changes

---
+++ src/Hello.js
@@ -0,0 +1,25 @@
+// import React, { Component } from "react";
+import "./Hello.css";
+// class Hello extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world</h1>
+//         <p>Welcome to React</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Hello;
+
+function Hello() {
+  return (
+    <div className="f1 tc">
+      <h1>Hello, world</h1>
+      <p>Welcome to React</p>
+    </div>
+  );
+}
+
+export default Hello;
